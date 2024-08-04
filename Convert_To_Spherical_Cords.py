562d21306685eb8dd5858ffaabb42fef296ea586
Drake S. <47800977+Oakleypaws@users.noreply.github.com>
2024-08-04 12:25:28 -0400

    Add files via upload

--- Convert_To_Spherical_Cords.py
+++ Convert_To_Spherical_Cords.py
@@ -1,12 +1,13 @@
-from SpericalCord import SpericalCord
+from SpericalCord import SpericalCord,SphericalSquareCord
 
 fileName = input("What file would you like to change? ")
+
 try:
     file = open(fileName, "at")
 
     addAnother = True
     while addAnother == True:
-        User_input = input("\nWould you like to add another coordinate? (y/n)")
+        User_input = input("\nWould you like to add another coordinate?\nYes: Y\nNo: N\nSquare: S\n")
         if User_input.upper() == "N":
             addAnother = False
         elif User_input.upper() == "Y":
@@ -33,9 +34,44 @@ try:
             
             file.write(SCord1.getSphericalCords() + "/" + SCord2.getSphericalCords() + "\n")
             
+        elif User_input.upper() == "S":
+            
+            validInput = False
+            while validInput == False:
+                input1 = input("\nPlease provide a corner of the square in x,y,z form: ")
+                cord1 = input1.split(",")
+                
+                if len(cord1) >= 3:
+                    validInput = True
+            
+            validInput = False
+            while validInput == False:
+                input2 = input("\nPlease provide a distance from the corner in x,y,z form: ")
+                cord2 = input2.split(",")
+                
+                if len(cord2) >= 3:
+                    validInput = True
+            
+            if float(cord2[0]) > 0:
+                if float(cord2[1]) > 0:
+                    square = SphericalSquareCord(float(cord1[0]), float(cord1[1]), float(cord1[2]), float(cord2[0]), float(cord2[1]), "xy")
+                elif float(cord2[2]) > 0:
+                    square = SphericalSquareCord(float(cord1[0]), float(cord1[1]), float(cord1[2]), float(cord2[0]), float(cord2[2]), "xz")
+                else:
+                    print("There was an error")
+            elif float(cord2[1]) > 0:
+                if float(cord2[2]) > 0:
+                    square = SphericalSquareCord(float(cord1[0]), float(cord1[1]), float(cord1[2]), float(cord2[1]), float(cord2[2]), "yz")
+                else:
+                    print("There was an error")
+            
+            file.write(square.getSphericalCords() + "\n")
+            
         else:
             print("\nThere was an error please try again")
 
     file.close()
+
 except:
     print("There was an error with the file\n")
+
